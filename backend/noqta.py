c9bbced3ff09a8dc08afcbf20da87c91513b7792
Enas <lap80053@gmail.com>
2025-11-30 16:51:54 +0300

    githup

--- backend/noqta.py
+++ backend/noqta.py
@@ -1,33 +1,4 @@
-"""TAX = 0.15
 
-Services = [
-    ("doritos" , 9),
-    ("lays" , 7),
-    ("pringles" , 12),
-    ("cheetos" , 8),
-    ("takis" , 5),
-]
-
-print("products:")
-for i, (name, price) in enumerate(Services, start=1):
-    print(i, "-", name)
-
-num = input("Enter product number ").strip()
-
-if not num.isdigit():
-    print("Enter number")
-else:
-    idx = int(num)
-    if 1 <= idx <= len(Services):
-        name, base_price = Services[idx - 1]
-        total = base_price * (1 + TAX)
-        print(f"product: {name}")
-        print(f"price before tax: {base_price:.2f}")
-        print(f"price after tax: {total:.2f}")
-    else:
-        print("Number out of range. Try a number between 1 and", len(Services))"""
-
- 
 TAX = 0.15
 
 
